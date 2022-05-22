a97fdc2bb52f5a82bca70832248ae49641c71bb4
Victor Ochoa <vochoa.developer@gmail.com>
2022-05-22 11:19:15 -0400

    Create main javascript file with functions

---
+++ calculator.js
@@ -0,0 +1,31 @@
+const add = function (a, b) {
+    return a + b;
+};
+
+const subtract = function (a, b) {
+    return a - b;
+};
+
+const multiply = function (a, b) {
+    return a * b;
+};
+
+const divide = function (a, b) {
+    return a / b;
+};
+
+const operate = function (op, a, b) {
+    switch (op) {
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case '*':
+            return multiply(a, b);
+        case '/':
+            return divide(a, b);
+        default:
+            return "Invalid operation";
+    };
+};
+
